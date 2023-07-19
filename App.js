20a3aad147f36499cb226f78b267b6fb0e87d566
muneebahmadbhat <126461917+muneebahmadbhat@users.noreply.github.com>
2023-07-19 17:45:04 +0530

    after using react router

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 // import logo from './logo.svg';
 import { useState } from 'react';
 import './App.css';
-// import About from './component/About';
+import About from './component/About';
 import Navbar from './component/Navbar';
 import TextForm from './component/TextForm';
-import Alert from './component/Alert';
+import Alert from './component/Alert';                            
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [mode,setMode] = useState('light');  //Whether Dark Mode is enabled or not
+  const [mode2,setMode2] = useState('light');  //Whether Red Mode is enabled or not
+  const [mode3,setMode3] = useState('light');  //Whether Green Mode is enabled or not
   const [alert,setAlert] =  useState(null);
 
   const showAlert = (message, type)=>{
@@ -25,8 +33,35 @@ const toggleMode = () =>{
     setMode('dark');
     document.body.style.backgroundColor = '#042743';
     showAlert("Dark mode has been enabled", "success ");
-   }else{
+   }
+   else{
     setMode('light');
+    document.body.style.backgroundColor = 'white';
+    showAlert("Light mode has been enabled", "success ");
+   }
+};
+
+const toggleMode2 = () =>{
+  if(mode2==='light'){
+    setMode2('red');
+    document.body.style.backgroundColor = '#ff0000';
+    showAlert("Red mode has been enabled", "success ");
+   }
+   else{
+    setMode2('light');
+    document.body.style.backgroundColor = 'white';
+    showAlert("Light mode has been enabled", "success ");
+   }
+}
+
+const toggleMode3 = () =>{
+  if(mode3==='light'){
+    setMode3('green');
+    document.body.style.backgroundColor = '#00ff00';
+    showAlert("Green mode has been enabled", "success ");
+   }
+   else{
+    setMode3('light');
     document.body.style.backgroundColor = 'white';
     showAlert("Light mode has been enabled", "success ");
    }
@@ -37,13 +72,20 @@ const toggleMode = () =>{
 {/* <Navbar title ="TextUtils" aboutText = "About US"/> */}
 {/* <Navbar/> */}
 
-<Navbar title="TextUtils" mode = {mode} toggleMode={toggleMode} />
+    <Router>
+<Navbar title="TextUtils" mode = {mode} toggleMode={toggleMode} mode2 ={mode2} toggleMode2={toggleMode2} mode3 = {mode3} toggleMode3={toggleMode3} />
 <Alert alert={alert}/>
 
   <div className="container my-3">
-{/* <About/> */}
-<TextForm showAlert={showAlert} heading= "Enter the text to analyze below" mode = {mode}/>
-  </div>
+  <Routes>   
+  {/* //users --> Component 1
+      //user2/home --> Component
+   */}
+          <Route exact path="/about" element={<About/>} />
+          <Route exact path="/" element={<TextForm showAlert={showAlert} heading= "Enter the text to analyze below" mode = {mode}/>} />
+        </Routes>
+      </div>
+</Router>
    </>
   );
 }
