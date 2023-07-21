669d0f3a13396af2072f0b1572dea8e341f5d6c6
muneebahmadbhat <126461917+muneebahmadbhat@users.noreply.github.com>
2023-07-21 18:26:18 +0530

    After fixing some errors

--- TextForm.js
+++ TextForm.js
@@ -46,23 +46,23 @@ export default function TextForm(props) {
     <div className='container' style={{color: props.mode==='dark'?'white':'#042743'}}> 
     <h1>{props.heading}</h1>
 <div className = "mb-3"> 
-  <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white',color: props.mode==='dark'?'white':'#042743'}}id="myBox" rows="8"></textarea>
+  <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#13466e':'white',color: props.mode==='dark'?'white':'#042743'}}id="myBox" rows="8"></textarea>
 </div>
-<button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
-<button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
-<button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
-<button className="btn btn-primary mx-2" onClick={Speak}>Speak</button>
-<button type="submit" className="btn btn-danger mx-2" onClick={cancelSpeech}>Stop TTS</button>
+<button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+<button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
+<button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
+<button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={Speak}>Speak</button>
+<button disabled={text.length===0} type="submit" className="btn btn-danger mx-1 my-1" onClick={cancelSpeech}>Stop TTS</button>
     </div>
     <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
         <h2>Your text summary</h2>
         
-        <p>{text.trim() === '' ? 0 : text.match(/\S+/g).length} words and {text.replace(/\s+/g, '').length} characters</p> 
+        <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.replace(/\s+/g, '').length} characters</p> 
 
 {/* <p>{text.split(" ").length} word and {text.length} characters</p> */}
-        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
         <h2>Preview</h2>
-        <p>{text.length>0?text:"Enter something in the textBox above to preview here"}</p>
+        <p>{text.length>0?text:"Nothing to preview here"}</p>
     </div>
     </>
   )
